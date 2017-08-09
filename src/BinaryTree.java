c976aba103d32d26aca09ac1e15af151301a5462
icygub <jchubbs95@gmail.com>
2017-08-08 22:38:13 -0600

    fixed bug in findNode()

--- src/BinaryTree.java
+++ src/BinaryTree.java
@@ -6,29 +6,66 @@ public class BinaryTree {
         root = null;
     }
 
+    public void insertNode(int data) {
+        if(isEmpty()) {
+            root = new Node(data);
+            System.out.println("Root node added");
+        }
+        else
+            insertNode(data, root);
+
+    }
+
     public void insertNode(int data, Node recursiveNode) { //recursiveNode should be root node when initally called
         Node node = new Node(data);
-        if(isEmpty()) {
-            recursiveNode = node;
-        } else if(node.data < recursiveNode.data) {
+        if(node.data < recursiveNode.data) {
             if(recursiveNode.left == null)
-                recursiveNode = node;
+                recursiveNode.left = node;
             else
                 insertNode(node.data, recursiveNode.left);
         } else {
             if(recursiveNode.right == null)
-                recursiveNode = node;
+                recursiveNode.right = node;
             else
                 insertNode(node.data, recursiveNode.right);
         }
     }
 
     public void deleteNode(int data) {
-
+        deleteNode(data, root);
     }
 
-    public boolean findNode(int data) {
-        return false;
+    public void deleteNode(int data, Node recursiveNode) {
+        //this will probably be the most difficult method to implement
+    }
+
+    public void findNode(int data) {
+        if(isEmpty())
+            System.out.println("The binary tree is empty");
+        else if(findNode(data, root))
+            System.out.printf("Node(%d) found!%n", data);
+        else
+            System.out.printf("Node(%d) does not exist.%n", data);
+    }
+
+    public boolean findNode(int data, Node recursiveNode) {
+        boolean nodeFound;
+
+        if(data == recursiveNode.data) {
+            nodeFound = true;
+        } else if(data < recursiveNode.data) {
+            if(recursiveNode.left == null) {
+                return false;
+            } else
+                nodeFound = findNode(data, recursiveNode.left);
+        } else {
+            if(recursiveNode.right == null) {
+                return false;
+            } else
+                nodeFound = findNode(data, recursiveNode.right);
+        }
+
+        return nodeFound;
     }
 
     public void inorderTraversal() {
--- src/BinaryTreeTest.java
+++ src/BinaryTreeTest.java
@@ -3,6 +3,29 @@ import static org.junit.Assert.*;
 public class BinaryTreeTest {
     @org.junit.Test
     public void insertNode() throws Exception {
+        BinaryTree binaryTree = new BinaryTree();
+        binaryTree.insertNode(5);
+        binaryTree.insertNode(4);
+        binaryTree.insertNode(3);
+        binaryTree.insertNode(2);
+        binaryTree.insertNode(7);
+        binaryTree.insertNode(6);
+        binaryTree.insertNode(8);
+        binaryTree.insertNode(9);
+        binaryTree.insertNode(1);
+        binaryTree.insertNode(0);
+
+        binaryTree.findNode(5);
+        binaryTree.findNode(4);
+        binaryTree.findNode(3);
+        binaryTree.findNode(2);
+        binaryTree.findNode(7);
+        binaryTree.findNode(6);
+        binaryTree.findNode(8);
+        binaryTree.findNode(9);
+        binaryTree.findNode(1);
+        binaryTree.findNode(0);
+
     }
 
     @org.junit.Test
